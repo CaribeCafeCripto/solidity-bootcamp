80b6dc75fe8db248a20ab955d74026f7fdd68754
criptorockstar <criptorockstar@gmail.com>
2024-03-07 10:54:20 -0300

    [update] 1.3.funciones

--- 1.fundamentos/1.3.funciones/contracts/funciones.sol
+++ 1.fundamentos/1.3.funciones/contracts/funciones.sol
@@ -4,14 +4,8 @@ pragma solidity ^0.8.19;
 contract Funciones {
     /*@Tipos de funciones
 
-    public: pueden ser llamadas desde dentro y fuera
-    del contrato sin ningun tipo de restriccion.
-
     internal: solo pueden ser llamadas desde dentro del mismo contrato
     o por contratos heredados.
-
-    external: solo pueden ser llamadas desde fuera del contrato,
-    son utiles para interfaces con otros contratos.
 
     private: similar a internal pero estas no pueden ser accedidas
     por contratos heredados.
@@ -35,4 +29,35 @@ contract Funciones {
     abstract: Indica que la función no tiene una implementación y debe ser sobrescrita por las subclases.
     se utiliza en interfaces y en clases abstractas.
     */
+
+    uint public numero;
+
+   // Modificadores de visibilidad
+
+   /*public: pueden ser llamadas desde dentro y fuera
+    del contrato sin ningun tipo de restriccion.*/
+    function setPublico(uint _numero) public {
+        numero = _numero;
+    }
+
+    /*external: solo pueden ser llamadas desde fuera del contrato,
+    son utiles para interfaces con otros contratos.*/
+
+    function setExternal(uint _numero) external {
+        numero = _numero;
+    }
+
+    function sumarPublico(uint _sumar) public {
+        /* Funcionara porque estamos usando 'this.'
+        para hacer la llamada a una funcion externa*/
+        this.setExternal(numero + _sumar);
+    }
+
+    function multiplicar() public {
+        /* No funcinara porque estamos intentando hacer una llamada interna
+        a una funcion externa*/
+
+        /* Descomentar la siguiente linea causara un error de compilacion*/
+        //setExternal(numero * 2);
+    }
 }
